c195ed2f92907cf0a40e9ad5221972b3b391e895
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-10-19 16:32:07 -0500

    add UT fakegen guards by default

--- src/hooks/Helpers.py
+++ src/hooks/Helpers.py
@@ -8,14 +8,23 @@ if TYPE_CHECKING:
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the category, False to disable it, or None to use the default behavior
 def before_is_category_enabled(multiworld: MultiWorld, player: int, category_name: str) -> Optional[bool]:
+    if hasattr(multiworld, "generation_is_fake"):
+        return None
+
     return None
 
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the item, False to disable it, or None to use the default behavior
 def before_is_item_enabled(multiworld: MultiWorld, player: int, item: "ManualItem") -> Optional[bool]:
+    if hasattr(multiworld, "generation_is_fake"):
+        return None
+
     return None
 
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the location, False to disable it, or None to use the default behavior
 def before_is_location_enabled(multiworld: MultiWorld, player: int, location: "ManualLocation") -> Optional[bool]:
+    if hasattr(multiworld, "generation_is_fake"):
+        return None
+
     return None
